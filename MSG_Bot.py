a734d15ee68d8c1e869d025ace5a603561f627f2
ffstghc <Frank.Filip@protonmail.com>
2022-08-06 19:56:15 +0200

    Small Changes

--- MSG_Bot.py
+++ MSG_Bot.py
@@ -9,9 +9,9 @@ from datetime import timedelta, datetime
 from dateutil import parser
 
 
-######################
-## IMPORT VARIABLES ##
-######################
+###########################################
+## IMPORT BOT API CONNECTION INFORMATION ##
+###########################################
 from Non_Public_Info import Bot_Auth_Token, Bot_Channel_ChatID  # Get Info from non public .PY-file to connect to Bot
 
 
@@ -38,8 +38,8 @@ tags_other = ["Bioinformatics", "Pharmacometrics"]
 
 def Send_To_Channel(Message):
 
-    bot_token = Bot_Auth_Token  # Hidden Token; Replace with own
-    bot_chatID = Bot_Channel_ChatID  # Hidden Chat-ID; Replace with own
+    bot_token = Bot_Auth_Token  # Hidden Token
+    bot_chatID = Bot_Channel_ChatID  # Hidden Chat-ID
 
     send_text = 'https://api.telegram.org/bot' + bot_token + \
                 '/sendMessage?chat_id=' + bot_chatID + \
@@ -62,6 +62,8 @@ titles = ["Doktorand", "Doctoral",
 
 def main(URL, Limit_Hours):  # Get RSS data from URL List
 
+    found = []
+
     rss_data = feedparser.parse(URL)  # Get RSS Feed Data
 
     for entry in rss_data.entries:  # Check all available RSS Information
@@ -82,28 +84,37 @@ def main(URL, Limit_Hours):  # Get RSS data from URL List
                       + entry.links[0].href + "\n" \
                       + "Published: " + date_published
 
-                print(msg, "\n")  # Output Title + Date to Console
+                found.append(msg)   # Save Positions in List
 
-                #################################
-                ## UNCOMMENT HERE TO BROADCAST ##
-                #################################
+    return found
 
-                # Send_To_Channel(msg)  # Output Title + Date to Telegram Channel as 1st Message
-
-                # sleep(4)  # Timer to avoid 30 msg/min Limit
 
 
 ###############################################################
 ## RUN SEARCH FOR LIST OF URLS AND SPECIFIED UPDATE INTERVAL ##
 ###############################################################
-Update_Interval_hours = 1  # 168 = 1 Week; 680 1 Month
+Update_Interval_hours = 900  # 168 = 1 Week; 680 1 Month
 Update_Interval_seconds = Update_Interval_hours * 60 * 60  # Convert to Seconds for Sleep Function
 
 if __name__ == "__main__":
+    print("Bot started. Checking", len(URLs), "URLs every", Update_Interval_hours, "hours")
     while (True):
         for URL in URLs:    # Loop all provided URLs
-            main(URL, Update_Interval_hours)
+
+            found_pos = main(URL, Update_Interval_hours)    # Open Positions within single Institution
+
+            for positions in range(len(found_pos)):
+                print(found_pos[positions], "\n") # Output Title + Date + Link to Console
+
+                #################################
+                ## UNCOMMENT HERE TO BROADCAST ##
+                #################################
+
+                # Send_To_Channel(found_pos)  # Output Title + Date + Link to Telegram Channel
+                # sleep(4)  # Timer to avoid 30 msg/min Limit
+
+        print("All found Positions printed")
 
         for i in range(Update_Interval_seconds, 0, -5):
-            sleep(5) # Don't Spam Console (too much)
+            sleep(60) # Don't Spam Console (too much)
             print("Remaining Seconds until next Check: ", i)  # Show Countdown until next Check
